259b8ff01bbccf299c64ae395ef8d9550ffc3a77
Yodyeam Autanuwat <meen41813@hotmail.com>
2018-06-03 16:42:15 +0700

    edit1

--- Calculator.js
+++ Calculator.js
@@ -1,9 +1,9 @@
-class Calculator{
-    calculate(str){
-        if (str == ''){
-            return 0;
-        }
+class Calculator {
+  calculate(str) {
+    if (str === '') {
+      return 0;
     }
+  }
 }
 
 module.exports = Calculator;
