3dee7fbab0e54cc1e662139f85c9f70721e8466b
Yodyeam Autanuwat <meen41813@hotmail.com>
2018-06-04 16:10:49 +0700

    summation

--- Calculator.js
+++ Calculator.js
@@ -1,9 +1,26 @@
 class Calculator {
   calculate(str) {
+    var arr = str.split(',');
+    var arr2 = str.split('\n');
     if (str === '') {
       return 0;
     }
+    else if (Number.isInteger(str) === true) {
+      return str;
+    }
+    else if (str.indexOf(',') >= 0) {
+      // var arr = str.split(',');
+      // console.log(parseInt(arr[0]),parseInt(arr[1]))
+      // console.log(parseInt(arr[0])+parseInt(arr[1]))
+      return parseInt(arr[0], 10) + parseInt(arr[1], 10);
+    }
+    else if (str.indexOf('\n') >= 0) {
+      // var arr2 = str.split('\n');
+      // console.log(parseInt(arr[0]),parseInt(arr[1]))
+      // console.log(parseInt(arr[0])+parseInt(arr[1]))
+      return parseInt(arr2[0], 10) + parseInt(arr2[1], 10);
+    }
   }
 }
-
 module.exports = Calculator;
+
--- test/test-string-calculator.js
+++ test/test-string-calculator.js
@@ -18,17 +18,38 @@ function itShouldBeZero() {
   expect(cal1.calculate('')).to.be.equal(0);
 }
 
+function singleNumber() {
+  // console.log('>> n = ' + n);
+  const cal1 = new Calculator();
+  expect(cal1.calculate(2)).to.be.equal(2);
+  // expect(cal1.calculate(9)).to.be.equal(9);
+  // expect(cal1.calculate(5)).to.be.equal(5);
+  // expect(cal1.calculate(99)).to.be.equal(99);
+  // expect(cal1.calculate(-50)).to.be.equal(-50);
+}
+
+function sum() {
+  const cal1 = new Calculator();
+  expect(cal1.calculate('10,1')).to.be.equal(11);
+  // expect(cal1.calculate("0,-15")).to.be.equal(-15);
+  // expect(cal1.calculate("99,11")).to.be.equal(110);
+}
+
+function sum2() {
+  const cal1 = new Calculator();
+  expect(cal1.calculate('50\n10')).to.be.equal(60);
+}
 // Start Example Behaviors
 function exampleBehaviors() {
   it('should be true', itAlwaysTrue);
   it('should be equal 2', itAlwaysBe2);
+  it('should be zero', itShouldBeZero);
+  it('single number', singleNumber);
+  it('summation', sum);
+  it('summation2', sum2);
 }
 
-function testEmptyString() {
-  it('should be zero', itShouldBeZero);
-}
 
 // Start Describe
 describe('Example', exampleBehaviors);
-describe('Empty string should return zero', testEmptyString);
-
+// describe('Empty string should return zero', testEmptyString);
