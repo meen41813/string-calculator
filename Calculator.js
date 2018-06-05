2e79d7b4f5c83308a61d655f09b3b10e228b8fc8
Yodyeam Autanuwat <meen41813@hotmail.com>
2018-06-05 09:36:32 +0700

    edit4

--- Calculator.js
+++ Calculator.js
@@ -1,7 +1,5 @@
 class Calculator {
   calculate(str) {
-    var arr = str.split(',');
-    var arr2 = str.split('\n');
     if (str === '') {
       return 0;
     }
@@ -9,13 +7,13 @@ class Calculator {
       return str;
     }
     else if (str.indexOf(',') >= 0) {
-      // var arr = str.split(',');
+      var arr = str.split(',');
       // console.log(parseInt(arr[0]),parseInt(arr[1]))
       // console.log(parseInt(arr[0])+parseInt(arr[1]))
       return parseInt(arr[0], 10) + parseInt(arr[1], 10);
     }
     else if (str.indexOf('\n') >= 0) {
-      // var arr2 = str.split('\n');
+      var arr2 = str.split('\n');
       // console.log(parseInt(arr[0]),parseInt(arr[1]))
       // console.log(parseInt(arr[0])+parseInt(arr[1]))
       return parseInt(arr2[0], 10) + parseInt(arr2[1], 10);
